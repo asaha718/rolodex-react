27a0c086ac5f3b71f421502a497f2948bb9e2fcd
Anug Saha <anugsaha@gmail.com>
2021-11-14 13:23:30 -0500

    learn fetching data on react

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import "./App.css";
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      monsters: [
-        { name: "Frank", id: 1 },
-        { name: "Fracula", id: 2 },
-        { name: "Zombie", id: 3 },
-      ],
-    };
+    this.state = {monsters:[]};
   }
+
+  componentDidMount(){ 
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users=> this.setState({monsters: users})); 
+
+  }
+
   render() {
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => (
+        {this.state.monsters.map(monster => (
           <h1 key={monster.id}> {monster.name}</h1>
         ))}
       </div>
